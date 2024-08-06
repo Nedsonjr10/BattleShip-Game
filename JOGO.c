5c80b0424334781134d787a50d4e17b4885d970d
Nedson Nogueira <nedson.junior91@gmail.com>
2024-08-06 02:23:12 -0300

    Implementando logicas iniciais

---
+++ JOGO.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define TAMANHO_TABULEIRO 10
+#define MAXIMO_NAVIOS 5
+#define TAMANHO_HASH 100
+
+typedef struct {
+    int x;
+    int y;
+} Posicao;
+
+typedef struct {
+    Posicao inicio;
+    Posicao fim;
+    int tamanho;
+    bool afundado;
+} Navio;
+
+typedef struct {
+    int id_jogador;
+    Navio navios[MAXIMO_NAVIOS];
+    int acertos;
+    int erros;
+    char tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
+} Jogador;
+
+typedef struct No {
+    Jogador jogador;
+    struct No *proximo;
+} No;
+
+typedef struct NoNavio {
+    Navio navio;
+    struct NoNavio *anterior;
+    struct NoNavio *proximo;
+} NoNavio;
+
+typedef struct Acao {
+    Posicao pos;
+    bool acertou;
+    struct Acao *proxima;
+} Acao;
+
+typedef struct NoEvento {
+    Acao acao;
+    struct NoEvento *proximo;
+} NoEvento;
+
+typedef struct {
+    bool acertos[TAMANHO_HASH];
+} TabelaHash;
+
+// Criação do NO 
+No* criar_no(Jogador jogador) {
+    No *novo_no = (No *)malloc(sizeof(No));
+    novo_no->jogador = jogador;
+    novo_no->proximo = novo_no;
+    return novo_no;
+}
+
+Acao* criar_acao(Posicao pos, bool acertou) {
+    Acao *nova_acao = (Acao *)malloc(sizeof(Acao));
+    nova_acao->pos = pos;
+    nova_acao->acertou = acertou;
+    nova_acao->proxima = NULL;
+    return nova_acao;
+}
+
+NoEvento* criar_no_evento(Acao acao) {
+    NoEvento *novo_no = (NoEvento *)malloc(sizeof(NoEvento));
+    novo_no->acao = acao;
+    novo_no->proximo = NULL;
+    return novo_no;
+}
